445226f29c237aa1a60f3b5efc6e5af124b721cb
michaeladze <michael.kutateladze@yandex.ru>
2021-02-08 15:16:44 +0300

    asd

--- src/indexSPA.tsx
+++ src/indexSPA.tsx
@@ -1,10 +1,9 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import React from 'react';
-import App from './App';
 
 const Root = (props: any) => {
-  return <App />;
+  return <div className='graph_graph'> Graph </div>;
 };
 
 export default Root;
